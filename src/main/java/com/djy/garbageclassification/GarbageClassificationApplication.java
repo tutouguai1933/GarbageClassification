66b992983979f97a7e14c6adf86d47028f9b2ebc
djy1933 <1933208939@qq.com>
2024-05-18 00:17:50 +0800

    first commit

---
+++ src/main/java/com/djy/garbageclassification/GarbageClassificationApplication.java
@@ -0,0 +1,13 @@
+package com.djy.garbageclassification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbageClassificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarbageClassificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/djy/garbageclassification/GarbageClassificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djy.garbageclassification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbageClassificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
